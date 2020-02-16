1e9683b65900fc6fb1ca3d1521d59a40573bb26f
Ian Webb <eon.stella.artois@gmail.com>
2020-02-16 20:43:32 +0000

    inverse

---
+++ ian8.test.js
@@ -0,0 +1,19 @@
+
+
+const inverse = (a) => 1/a;
+
+describe('my tests', function() {
+
+  describe('my tests', function() {
+    test('inverse 1', () => {
+      expect(inverse(4)).toBe(0.25);
+    });
+
+    test('inverse 2', () => {
+      expect(inverse(10)).toBe(0.1);
+    });
+  });
+
+});
+
+
