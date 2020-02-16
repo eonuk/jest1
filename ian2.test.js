fe7e6197368f0aa0a781ed1ad9f1b1028f6ca67f
Ian Webb <eon.stella.artois@gmail.com>
2020-02-16 14:35:27 +0000

    second changes

--- ian2.test.js
+++ ian2.test.js
@@ -12,17 +12,17 @@ describe('my tests', function() {
     });
 
     test('Outputs IV', () => {
-      //expect(func(5)).toBe('IV');
+      expect(func(5)).toBe(10);
     });
   });
   
   describe('my tests', function() {
     test('Outputs III', () => {
-      //expect(func(3)).toBe('III');
+      expect(func(3)).toBe(6);
     });
 
     test('Outputs IV', () => {
-      //expect(func(5)).toBe('IV');
+      expect(func(5)).toBe(10);
     });
   });
   
