3c311d966a70a78c33d724e5762176ac8bb59ac0
Ian Webb <eon.stella.artois@gmail.com>
2020-02-16 20:42:18 +0000

    squareroot

---
+++ ian7.test.js
@@ -0,0 +1,19 @@
+
+
+const sqrt = (a) => Math.sqrt(a);
+
+describe('my tests', function() {
+
+  describe('my tests', function() {
+    test('sqrt 1', () => {
+      expect(sqrt(9)).toBe(3);
+    });
+
+    test('sqrt 2', () => {
+      expect(sqrt(16)).toBe(4);
+    });
+  });
+
+});
+
+
